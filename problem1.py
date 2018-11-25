e7dbe8f6c0633bf1d87f80f6d7ff47cb9df5629b
John Petrella <johnepetrella@gmail.com>
2018-11-25 08:34:02 -0500

    add subtract function

--- problem1.py
+++ problem1.py
@@ -3,12 +3,17 @@
 def add(x,y):
     return x+y
 
+def subtract(x,y):
+    retunr x-y
+
 def main():
     sum = 0 
     for i in range(1,1000):
         if i % 3 == 0 or i % 5 == 0:
             sum = add(sum,i)
     print(sum)
+
+
 
 
 if __name__ == "__main__":
